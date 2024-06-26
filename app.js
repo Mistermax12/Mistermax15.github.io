7016f2a1d46bbe2e3b64377cf2ad92ba12235f74
Mistermax12 <90029651+Mistermax12@users.noreply.github.com>
2024-06-26 22:50:47 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-// Здесь пока нет ничего, но может быть полезен для будущего расширения
+document.querySelector('.claim-button').addEventListener('click', function() {
+  alert('Claim button clicked!');
+});
+
+document.querySelectorAll('.circle-button').forEach(button => {
+  button.addEventListener('click', function() {
+    alert(`${button.textContent} button clicked!`);
+  });
+});
