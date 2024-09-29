e7043300a32f3d5cabeeb2c574040ba8a5a5e706
Mistermax12 <90029651+Mistermax12@users.noreply.github.com>
2024-09-29 14:28:12 +0300

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-document.querySelector('.claim-button').addEventListener('click', function() {
-  alert('Claim button clicked!');
-});
-
-document.querySelectorAll('.circle-button').forEach(button => {
-  button.addEventListener('click', function() {
-    alert(`${button.textContent} button clicked!`);
-  });
-});
